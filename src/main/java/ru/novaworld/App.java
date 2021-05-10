3130804fdeae196fb85c085924a9e388739e1991
asgorlov <asgorlov@outlook.com>
2021-05-10 21:38:39 +0300

    Changed pom file and moved resources

--- src/main/java/ru/novaworld/App.java
+++ src/main/java/ru/novaworld/App.java
@@ -4,6 +4,6 @@ public class App {
 
     public static void main(String[] args) {
         DetailingScript detailingScript = new DetailingScript();
-        detailingScript.execute("./resources");
+        detailingScript.execute("./src/main/resources");
     }
 }
